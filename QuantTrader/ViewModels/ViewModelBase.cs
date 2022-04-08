69d68f88cf355038f1aef109ece26c6dfb8eafa8
adonayg3 <adonayg3@outlook.com>
2022-04-08 11:34:31 +0100

    add folder structure.

---
+++ QuantTrader/ViewModels/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ReactiveUI;
+
+namespace QuantTrader.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
